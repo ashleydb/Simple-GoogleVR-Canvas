bee1c98e84a0cf1b31449439e0c8f40e6752a6b3
ashleydb <abennett@gmail.com>
2016-07-28 13:08:11 -0700

    Initial Commit
    First commit with a basic scene to demonstrate usage of GoogleVR SDK for
    Unity.

---
+++ Assets/Cube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+    private GvrAudioSource audio_source_;
+
+    void OnEnable() {
+        audio_source_ = GetComponent<GvrAudioSource>();
+    }
+
+    public void OnSelect()
+    {
+        PlaySound();
+    }
+
+    void PlaySound()
+    {
+        if (audio_source_ == null)
+            return;
+        
+        audio_source_.Play();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+A simple scene setup in Unity with the minimal settings which demonstrates setting up:
+- A VR stereo camera with a reticule
+- A UI Canvas with a button to click
+- An object that plays spacial audio in the world
+
+Look around the world, click the button and hear audio come from the cube.
+
+Using Unity 3.5.6 and GoogleVR SDK v0.8.5.
